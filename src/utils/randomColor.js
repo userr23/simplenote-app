9328910b8cbeee5c7c8aa4381ede283dc5e11a24
userr23 <aavvsskr@gmail.com>
2019-11-12 20:13:08 +0200

    some changes

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -5,12 +5,12 @@ import { getRandomArbitrary, getRGBA } from './util';
  *
  * Description. description
  *
- * @param {string}  colorRGBA     Description. Base color.
+ * @param {string}  colorRGBA    Description.
  *
- * @return {string} Description. Random color.
+ * @return {string} Description.
  */
 export default function randomColor ( colorRGBA ) {
     const { red, green, blue, alpha } = getRGBA( colorRGBA );
-    return `rgba(${red * getRandomArbitrary( 0.97, 1 )}, ${green * getRandomArbitrary( 0.93, 1 )}, ${blue *
-    getRandomArbitrary( 0.99, 1 )}, ${alpha})`;
+    return `rgba(${red * getRandomArbitrary( 0.98, 1.05 )}, ${green * getRandomArbitrary( 0.95, 0.97 )}, ${blue *
+    getRandomArbitrary( 0.98, 1.05 )}, ${alpha})`;
 }
